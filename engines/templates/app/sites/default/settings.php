7832a3a874cce4e540e747737f23b65b6b851fac
James Vodanovich <james@jvodan.com>
2015-01-17 15:30:41 +1100

    first commit

--- engines/templates/app/sites/default/settings.php
+++ engines/templates/app/sites/default/settings.php
@@ -133,7 +133,7 @@ $drupal_hash_salt = '';
  * It is not allowed to have a trailing slash; Drupal will add it
  * for you.
  */
-$base_url = 'http://_ENGINES_fqdn';  // NO trailing slash!
+$base_url = 'http://_ENGINES_fqdn';  
 
 /**
  * PHP settings:
